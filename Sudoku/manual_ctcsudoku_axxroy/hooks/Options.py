0fb378d71f4bd5aeda757b5f9e7dea226b2feb97
Sam Zimmerman <samzimmerman@me.com>
2024-06-09 10:16:08 -0700

    Fix hooks

--- Sudoku/manual_ctcsudoku_axxroy/hooks/Options.py
+++ Sudoku/manual_ctcsudoku_axxroy/hooks/Options.py
@@ -1,5 +1,5 @@
 # Object classes from AP that represent different types of options that you can create
-from Options import FreeText, NumericOption, Toggle, DefaultOnToggle, Choice, TextChoice, Range
+from Options import FreeText, NumericOption, Toggle, DefaultOnToggle, Choice, TextChoice, Range, NamedRange
 
 # These helper methods allow you to determine if an option has been set, or what its value is, for any player in the multiworld
 from ..Helpers import is_option_enabled, get_option_value
@@ -7,7 +7,7 @@ from ..Helpers import is_option_enabled, get_option_value
 
 
 ####################################################################
-# NOTE: At the time that options are created, Manual has no concept of the multiworld or its own world. 
+# NOTE: At the time that options are created, Manual has no concept of the multiworld or its own world.
 #       Options are defined before the world is even created.
 #
 # Example of creating your own option:
@@ -26,24 +26,16 @@ from ..Helpers import is_option_enabled, get_option_value
 # To add an option, use the before_options_defined hook below and something like this:
 #   options["total_characters_to_win_with"] = TotalCharactersToWinWith
 #
-class GameDuration(Range):
-    """Expected game duration in hours"""
-    display_name = "Expected Game Duration (hours)"
-    range_start = 2
-    range_end = 10
-    default = 4
+class TotalCharactersToWinWith(Range):
+    """Instead of having to beat the game with all characters, you can limit locations to a subset of character victory locations."""
+    display_name = "Number of characters to beat the game with before victory"
+    range_start = 10
+    range_end = 50
+    default = 50
 
-class ExtraKeysRatio(Range):
-    """Ratio of total keys to required keys"""
-    display_name = "Extra keys ratio"
-    range_start = 0
-    range_end = 100
-    default = 25
 
 # This is called before any manual options are defined, in case you want to define your own with a clean slate or let Manual define over them
 def before_options_defined(options: dict) -> dict:
-    options["game_duration"] = GameDuration
-    options["extra_keys"] = ExtraKeysRatio
     return options
 
 # This is called after any manual options are defined, in case you want to see what options are defined or want to modify the defined options
