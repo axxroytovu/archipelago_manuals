6ca09cc50034d9a4e88e97a846476c77eaf818d3
Sam Zimmerman <samzimmerman@me.com>
2024-10-27 21:49:15 -0700

    Upgrade to current source

--- Slime Rancher/manual_slimerancher_axxroy/Helpers.py
+++ Slime Rancher/manual_slimerancher_axxroy/Helpers.py
@@ -1,12 +1,28 @@
-from BaseClasses import MultiWorld, Item
-from typing import Optional, List
+import os
+import pkgutil
+import json
+
+from BaseClasses import MultiWorld, Item, Location
+from typing import Optional, List, TYPE_CHECKING
 from worlds.AutoWorld import World
-from .Data import category_table
-from .Items import ManualItem
-from .Locations import ManualLocation
 from .hooks.Helpers import before_is_category_enabled, before_is_item_enabled, before_is_location_enabled
 
 from typing import Union
+
+if TYPE_CHECKING:
+    from .Items import ManualItem
+    from .Locations import ManualLocation
+
+# blatantly copied from the minecraft ap world because why not
+def load_data_file(*args) -> dict:
+    fname = os.path.join("data", *args)
+
+    try:
+        filedata = json.loads(pkgutil.get_data(__name__, fname).decode())
+    except:
+        filedata = []
+
+    return filedata
 
 def is_option_enabled(multiworld: MultiWorld, player: int, name: str) -> bool:
     return get_option_value(multiworld, player, name) > 0
@@ -28,14 +44,18 @@ def clamp(value, min, max):
         return value
 
 def is_category_enabled(multiworld: MultiWorld, player: int, category_name: str) -> bool:
+    from .Data import category_table
     """Check if a category has been disabled by a yaml option."""
     hook_result = before_is_category_enabled(multiworld, player, category_name)
     if hook_result is not None:
         return hook_result
 
     category_data = category_table.get(category_name, {})
-    if "yaml_option" in category_data:
-        for option_name in category_data["yaml_option"]:
+    return resolve_yaml_option(multiworld, player, category_data)
+
+def resolve_yaml_option(multiworld: MultiWorld, player: int, data: dict) -> bool:
+    if "yaml_option" in data:
+        for option_name in data["yaml_option"]:
             required = True
             if option_name.startswith("!"):
                 option_name = option_name[1:]
@@ -53,7 +73,7 @@ def is_item_name_enabled(multiworld: MultiWorld, player: int, item_name: str) ->
 
     return is_item_enabled(multiworld, player, item)
 
-def is_item_enabled(multiworld: MultiWorld, player: int, item: ManualItem) -> bool:
+def is_item_enabled(multiworld: MultiWorld, player: int, item: "ManualItem") -> bool:
     """Check if an item has been disabled by a yaml option."""
     hook_result = before_is_item_enabled(multiworld, player, item)
     if hook_result is not None:
@@ -69,7 +89,7 @@ def is_location_name_enabled(multiworld: MultiWorld, player: int, location_name:
 
     return is_location_enabled(multiworld, player, location)
 
-def is_location_enabled(multiworld: MultiWorld, player: int, location: ManualLocation) -> bool:
+def is_location_enabled(multiworld: MultiWorld, player: int, location: "ManualLocation") -> bool:
     """Check if a location has been disabled by a yaml option."""
     hook_result = before_is_location_enabled(multiworld, player, location)
     if hook_result is not None:
@@ -89,9 +109,12 @@ def _is_manualobject_enabled(multiworld: MultiWorld, player: int, object: any) -
 
     return enabled
 
-def get_items_for_player(multiworld: MultiWorld, player: int) -> List[Item]:
+def get_items_for_player(multiworld: MultiWorld, player: int, includePrecollected: bool = False) -> List[Item]:
     """Return list of items of a player including placed items"""
-    return [i for i in multiworld.get_items() if i.player == player]
+    items = [i for i in multiworld.get_items() if i.player == player]
+    if includePrecollected:
+        items.extend(multiworld.precollected_items.get(player, []))
+    return items
 
 def get_items_with_value(world: World, multiworld: MultiWorld, value: str, player: Optional[int] = None, force: bool = False) -> dict[str, int]:
     """Return a dict of every items with a specific value type present in their respective 'value' dict\n
@@ -101,7 +124,7 @@ def get_items_with_value(world: World, multiworld: MultiWorld, value: str, playe
     if player is None:
         player = world.player
 
-    player_items = get_items_for_player(multiworld, player)
+    player_items = get_items_for_player(multiworld, player, True)
     # Just a small check to prevent caching {} if items don't exist yet
     if not player_items:
         return {value: -1}
